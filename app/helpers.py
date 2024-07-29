1617e842aab080011314d9a89f98dc4fe27b9ae8
Varoon Kodithala <varoon.kodithala@gmail.com>
2024-07-29 18:37:23 -0400

    Adding Redis cache

--- app/helpers.py
+++ app/helpers.py
@@ -1,4 +1,5 @@
 from logging import Logger
+import redis
 import requests
 from typing import Annotated, Optional, Union
 from fastapi import Depends
@@ -9,6 +10,7 @@ from app import config, utils, tokenizer, main
 
 from sqlalchemy import make_url
 from llama_index.vector_stores.postgres import PGVectorStore
+from langchain_community.chat_message_histories import RedisChatMessageHistory
 
 
 import os
@@ -24,16 +26,13 @@ from llama_index.core.selectors import LLMSingleSelector
 
 from langchain_openai import ChatOpenAI
 from langchain_community.chat_message_histories import ChatMessageHistory
-from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import HumanMessage
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from llama_index.core import QueryBundle
-
-# Global variable to store chat histories
-store = {}
+from langchain_core.chat_history import BaseChatMessageHistory
 
 # Function to initialize the CustomSQLRetriever
 
@@ -42,34 +41,13 @@ def initialize_custom_retriever(db: Session):
     return utils.CustomSQLRetriever(db=db, embed_model=tokenizer)
 
 
-def initialize_and_get_response(session_id: str, user_input: str, logger: Logger,
-                                openai_apikey: str, langchain_apikey: str, cohere_api_key: str, enable_tracing: bool, db: Session) -> str:
+def initialize_and_get_response(user_phone: str, user_input: str, logger: Logger,
+                                openai_apikey: str, langchain_apikey: str, cohere_api_key: str, enable_tracing: bool, db: Session, redis_client: redis.Redis) -> str:
     # Set environment variables
     os.environ["LANGCHAIN_TRACING_V2"] = "true" if enable_tracing else "false"
     os.environ["LANGCHAIN_API_KEY"] = langchain_apikey
 
-    # Initialize the model
-    langchain_model = ChatOpenAI(
-        model="gpt-3.5-turbo", api_key=openai_apikey)  # type: ignore
-
-    def get_session_history(session_id: str) -> BaseChatMessageHistory:
-        if session_id not in store:
-            store[session_id] = ChatMessageHistory()
-        return store[session_id]
-
-    # Create a Runnable with message history
-    with_message_history = RunnableWithMessageHistory(
-        langchain_model, get_session_history)
-
-    # Define the prompt template for the second chatbot
-    prompt = ChatPromptTemplate.from_messages([
-        ("system", "You are a supportive therapist for reflective journaling, focusing on daily life, emotions, and personal events. You emphasize emotional exploration by asking specific, open-ended questions that help users understand and process their feelings deeply. The tone is empathetic, encouraging reflection and fostering self-discovery, without offering direct advice or solutions, instead facilitating personal insights and growth through thoughtful questioning. Keep the responses under 4 sentences and keep it to one question for a response."),
-        MessagesPlaceholder(variable_name="messages"),
-    ])
-
-    # Create the chain for the second chatbot
-    chain = prompt | langchain_model
-
+    # FIRST CHATBOT (RAG enabled)
     # Initialize the CustomSQLRetriever
     custom_retriever = initialize_custom_retriever(db)
     cohere_rerank = CohereRerank(api_key=cohere_api_key, top_n=3)
@@ -99,20 +77,31 @@ def initialize_and_get_response(session_id: str, user_input: str, logger: Logger
     """
     )
 
-    # Create chat engine for the first chatbot
-    chat_history = [
-        ChatMessage(
-            role=MessageRole.USER,
-            content="You are an assistant who specializes in helping me reflect on past events in my life. Use specific examples from the entries to explain the specifics behind the event that I am trying to reflect on.",
-        ),
-    ]
-
     chat_engine = CondenseQuestionChatEngine.from_defaults(
         query_engine=query_engine,
         condense_question_prompt=custom_prompt,
-        chat_history=chat_history,
         verbose=True,
     )
+
+    # SECOND CHATBOT (short-term memory enabled, RAG disabled)
+    # Initialize the model
+    langchain_model = ChatOpenAI(
+        model="gpt-3.5-turbo", api_key=openai_apikey)  # type: ignore
+
+    redis_history = main.get_session_history(redis_client, user_phone)
+    logger.info(redis_history)
+
+    system_message = f"""You are a supportive therapist for reflective journaling, focusing on daily life, emotions, and personal events. You emphasize emotional exploration by asking specific, open-ended questions that help users understand and process their feelings deeply. The tone is empathetic, encouraging reflection and fostering self-discovery, without offering direct advice or solutions, instead facilitating personal insights and growth through thoughtful questioning. Keep the responses under 4 sentences and keep it to one question for a response.
+    Here is a list of all of the past messages provided by the user:
+    {' '.join(redis_history)}"""  # type: ignore
+
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", system_message),
+    ])
+    logger.info(prompt)
+
+    # Create the chain for the second chatbot
+    chain = prompt | langchain_model
 
     # Choices for routing
     choices = [
@@ -122,36 +111,26 @@ def initialize_and_get_response(session_id: str, user_input: str, logger: Logger
 
     selector = LLMSingleSelector.from_defaults()
 
-    # Manage session history
-    if session_id not in store:
-        store[session_id] = ChatMessageHistory()
-
     # Use the selector to choose the chatbot
-    chat_history = store[session_id].messages
     selector_result = selector.select(choices, query=user_input)
-    selected_choice = selector_result.selections[0].index
+    # selected_choice = selector_result.selections[0].index
+    selected_choice = 1
 
     if selected_choice == 0:
-        # Use the first chatbot
-        chat_history.append(ChatMessage(
-            role=MessageRole.USER, content=user_input))
+        # Use the first chatbot (RAG enabled)
         response = str(chat_engine.chat(user_input))
-        chat_history.append(ChatMessage(
-            role=MessageRole.ASSISTANT, content=response))
     else:
-        # Use the second chatbot
+        # Use the second chatbot (RAG disabled)
         response = chain.invoke(
-            {"messages": [HumanMessage(content=user_input)]})
-        chat_history.append(ChatMessage(
-            role=MessageRole.ASSISTANT, content=response.content))
+            {"\"message\"": user_input})
         response = response.content
 
     return response  # type: ignore
 
 
 # todo: what you're gonna wanna work on Varun
-def generate_response(user_phone: str, content: str, date_sent: str, logger: Logger, settings: config.Settings, db: Session):
-    return user_phone + " " + initialize_and_get_response('1', content, logger, settings.openai_api_key, settings.langchain_api_key, settings.cohere_api_key, True, db)
+def generate_response(user_phone: str, content: str, date_sent: str, logger: Logger, settings: config.Settings, db: Session, redis_client: redis.Redis):
+    return user_phone + " " + initialize_and_get_response(user_phone, content, logger, settings.openai_api_key, settings.langchain_api_key, settings.cohere_api_key, True, db, redis_client)
 
 
 async def send_message(user_phone: str, response: str, settings: config.Settings):
