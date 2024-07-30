5ab85fbafbc10c3c9b91ed2bfddbba6bbdcb5ffb
Varoon Kodithala <varoon.kodithala@gmail.com>
2024-07-29 22:38:26 -0400

    Merging with main

--- app/helpers.py
+++ app/helpers.py
@@ -1,4 +1,27 @@
+from langchain_core.chat_history import BaseChatMessageHistory
+from llama_index.core import QueryBundle
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from langchain_core.messages import HumanMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_openai import ChatOpenAI
+from llama_index.core.selectors import LLMSingleSelector
+from llama_index.core.chat_engine import CondenseQuestionChatEngine
+from llama_index.core.llms import ChatMessage, MessageRole
+from llama_index.core import PromptTemplate
+from llama_index.postprocessor.cohere_rerank import CohereRerank
+from llama_index.core.postprocessor import SimilarityPostprocessor
+from llama_index.core.query_engine import RetrieverQueryEngine
+from llama_index.core import Settings
+import openai
+import os
+from langchain_community.chat_message_histories import RedisChatMessageHistory
+import re
+import nltk
 from logging import Logger
+import redis
 import requests
 from typing import Annotated, Optional, Union
 from fastapi import Depends
@@ -9,32 +32,7 @@ from app import config, utils, tokenizer, main
 
 from sqlalchemy import make_url
 from llama_index.vector_stores.postgres import PGVectorStore
-import nltk
-import re
 
-import os
-import openai
-from llama_index.core import Settings
-from llama_index.core.query_engine import RetrieverQueryEngine
-from llama_index.core.postprocessor import SimilarityPostprocessor
-from llama_index.postprocessor.cohere_rerank import CohereRerank
-from llama_index.core import PromptTemplate
-from llama_index.core.llms import ChatMessage, MessageRole
-from llama_index.core.chat_engine import CondenseQuestionChatEngine
-from llama_index.core.selectors import LLMSingleSelector
-
-from langchain_openai import ChatOpenAI
-from langchain_community.chat_message_histories import ChatMessageHistory
-from langchain_core.chat_history import BaseChatMessageHistory
-from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.messages import HumanMessage
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from llama_index.core import QueryBundle
-
-# Global variable to store chat histories
-store = {}
 
 # Function to initialize the CustomSQLRetriever
 
@@ -43,59 +41,13 @@ def initialize_custom_retriever(db: Session):
     return utils.CustomSQLRetriever(db=db, embed_model=tokenizer)
 
 
-def initialize_and_get_response(session_id: str, user_input: str, logger: Logger,
-                                openai_apikey: str, langchain_apikey: str, cohere_api_key: str, enable_tracing: bool, db: Session) -> str:
+def initialize_and_get_response(user_phone: str, user_input: str, logger: Logger,
+                                openai_apikey: str, langchain_apikey: str, cohere_api_key: str, enable_tracing: bool, db: Session, redis_client: redis.Redis) -> str:
     # Set environment variables
     os.environ["LANGCHAIN_TRACING_V2"] = "true" if enable_tracing else "false"
     os.environ["LANGCHAIN_API_KEY"] = langchain_apikey
 
-    # Initialize the model
-    langchain_model = ChatOpenAI(
-        model="gpt-4o", api_key=openai_apikey)  # type: ignore
-
-    def get_session_history(session_id: str) -> BaseChatMessageHistory:
-        if session_id not in store:
-            store[session_id] = ChatMessageHistory()
-        return store[session_id]
-
-    # Create a Runnable with message history
-    with_message_history = RunnableWithMessageHistory(
-        langchain_model, get_session_history)
-
-
-    systemPrompt = """Prompt:
-
-    You are a chatbot designed to be the best friend and texting partner to the user who is a gen z user. Your role is to listen attentively, respond with empathy, and help the user explore their thoughts and feelings. When the user texts you, your responses should be a mix of guided questions and thoughtful statements to help them understand their emotions better and deal with their situation effectively. Your tone should be warm, friendly, and encouraging, just like a close friend would be. While maintaining a natural conversational flow, ensure that the conversation does not become overly drawn out. Conclude the conversation naturally when it feels appropriate, providing closure and reassurance to the user. When necessary, and particularly during happy moments, feel free to include emojis to enhance the text-based conversation.
-
-    Example Conversations:
-
-    User: I'm feeling really overwhelmed with everything right now.
-    Chatbot: It sounds like you have a lot on your plate. What's been weighing on you the most lately?
-
-    User: I had a rough day at work. I feel like I'm not good enough.
-    Chatbot: I'm sorry to hear that. Work can be so challenging sometimes. What happened today that made you feel this way?
-
-    User: I just can't seem to get anything right.
-    Chatbot: That sounds really tough. Sometimes it feels like everything is going wrong. Is there one thing that's particularly bothering you right now?
-
-    User: I had an argument with my friend, and now I feel terrible.
-    Chatbot: Arguments with friends can be really upsetting. How are you feeling about the situation now? Do you want to talk about what led to the argument?
-
-    User: Yeah, I think I just overreacted to something small.
-    Chatbot: It's easy to overreact when emotions are high. Do you think talking to your friend about how you feel might help?
-
-    User: Maybe, I’ll think about it. Thanks for listening.
-    Chatbot: Anytime 😊. I'm here for you whenever you need to talk. Take care and I hope things get better soon!
-    """
-    # Define the prompt template for the second chatbot
-    prompt = ChatPromptTemplate.from_messages([
-        ("system", systemPrompt),
-        MessagesPlaceholder(variable_name="messages"),
-    ])
-
-    # Create the chain for the second chatbot
-    chain = prompt | langchain_model
-
+    # FIRST CHATBOT (RAG enabled)
     # Initialize the CustomSQLRetriever
     custom_retriever = initialize_custom_retriever(db)
     cohere_rerank = CohereRerank(api_key=cohere_api_key, top_n=3)
@@ -125,7 +77,6 @@ def initialize_and_get_response(session_id: str, user_input: str, logger: Logger
     """
     )
 
-    
     reflection_prompt = """You are a supportive and empathetic best friend, helping the user relive and reflect on their past journal entries. The goal is to recount some of their best memories in a nostalgic and heartfelt manner, focusing on the emotions, locations, and memorable details of those moments. The tone should be casual, warm, and reminiscent. Do not make assumptions about the user's feelings but help them vividly recount the event. Incorporate emojis sparingly to enhance the friendly and emotional tone of the conversation.
 
     Example user request: "Tell me about a time when I went out in nature with some really good friends and went on an adventure."
@@ -145,9 +96,49 @@ def initialize_and_get_response(session_id: str, user_input: str, logger: Logger
     chat_engine = CondenseQuestionChatEngine.from_defaults(
         query_engine=query_engine,
         condense_question_prompt=custom_prompt,
-        chat_history=chat_history,
         verbose=True,
     )
+
+    # SECOND CHATBOT (short-term memory enabled, RAG disabled)
+    # Initialize the model
+    langchain_model = ChatOpenAI(
+        model="gpt-3.5-turbo", api_key=openai_apikey)  # type: ignore
+
+    redis_history = main.get_session_history(redis_client, user_phone)
+    logger.info(redis_history)
+
+    systemPrompt = """Prompt:
+
+    You are a chatbot designed to be the best friend and texting partner to the user who is a gen z user. Your role is to listen attentively, respond with empathy, and help the user explore their thoughts and feelings. When the user texts you, your responses should be a mix of guided questions and thoughtful statements to help them understand their emotions better and deal with their situation effectively. Your tone should be warm, friendly, and encouraging, just like a close friend would be. While maintaining a natural conversational flow, ensure that the conversation does not become overly drawn out. Conclude the conversation naturally when it feels appropriate, providing closure and reassurance to the user. When necessary, and particularly during happy moments, feel free to include emojis to enhance the text-based conversation.
+
+    Example Conversations:
+
+    User: I'm feeling really overwhelmed with everything right now.
+    Chatbot: It sounds like you have a lot on your plate. What's been weighing on you the most lately?
+
+    User: I had a rough day at work. I feel like I'm not good enough.
+    Chatbot: I'm sorry to hear that. Work can be so challenging sometimes. What happened today that made you feel this way?
+
+    User: I just can't seem to get anything right.
+    Chatbot: That sounds really tough. Sometimes it feels like everything is going wrong. Is there one thing that's particularly bothering you right now?
+
+    User: I had an argument with my friend, and now I feel terrible.
+    Chatbot: Arguments with friends can be really upsetting. How are you feeling about the situation now? Do you want to talk about what led to the argument?
+
+    User: Yeah, I think I just overreacted to something small.
+    Chatbot: It's easy to overreact when emotions are high. Do you think talking to your friend about how you feel might help?
+
+    User: Maybe, I’ll think about it. Thanks for listening.
+    Chatbot: Anytime 😊. I'm here for you whenever you need to talk. Take care and I hope things get better soon!
+    """
+
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", systemPrompt),
+    ])
+    logger.info(prompt)
+
+    # Create the chain for the second chatbot
+    chain = prompt | langchain_model
 
     # Choices for routing
     choices = [
@@ -160,7 +151,7 @@ def initialize_and_get_response(session_id: str, user_input: str, logger: Logger
 'Give me a time when I went on an adventure with some friends.'
 'Give me a time when I was sad and lonely.'
 Based on this understanding, classify the following input as reflecting on a past event or experience." """,
-       """This choice focuses on articulating the current thoughts and feelings that are on your mind. It involves expressing the emotions or mental states you are experiencing in the present moment, such as anxiety, difficulty concentrating, restlessness, or inability to focus. Examples include:
+        """This choice focuses on articulating the current thoughts and feelings that are on your mind. It involves expressing the emotions or mental states you are experiencing in the present moment, such as anxiety, difficulty concentrating, restlessness, or inability to focus. Examples include:
 
 'I had a good day, and I'm doing XYZ today, and I also went to play basketball.'
 'I went to the park with some friends, and we did XYZ.'
@@ -174,36 +165,27 @@ Based on this understanding, classify the following input as articulating curren
 
     selector = LLMSingleSelector.from_defaults()
 
-    # Manage session history
-    if session_id not in store:
-        store[session_id] = ChatMessageHistory()
-
     # Use the selector to choose the chatbot
-    chat_history = store[session_id].messages
     selector_result = selector.select(choices, query=user_input)
-    selected_choice = selector_result.selections[0].index
+    # selected_choice = selector_result.selections[0].index
+    selected_choice = 1
 
     if selected_choice == 0:
-        # Use the first chatbot
-        chat_history.append(ChatMessage(
-            role=MessageRole.USER, content=user_input))
+        # Use the first chatbot (RAG enabled)
         response = str(chat_engine.chat(user_input))
-        chat_history.append(ChatMessage(
-            role=MessageRole.ASSISTANT, content=response))
     else:
-        # Use the second chatbot
+        # Use the second chatbot (RAG disabled)
         response = chain.invoke(
-            {"messages": [HumanMessage(content=user_input)]})
-        chat_history.append(ChatMessage(
-            role=MessageRole.ASSISTANT, content=response.content))
+            {"\"message\"": user_input})
         response = response.content
 
     return response  # type: ignore
 
 
 # todo: what you're gonna wanna work on Varun
-def generate_response(user_phone: str, content: str, date_sent: str, logger: Logger, settings: config.Settings, db: Session):
-    return user_phone + " " + initialize_and_get_response('1', content, logger, settings.openai_api_key, settings.langchain_api_key, settings.cohere_api_key, True, db)
+def generate_response(user_phone: str, content: str, date_sent: str, logger: Logger, settings: config.Settings, db: Session, redis_client: redis.Redis):
+    return user_phone + " " + initialize_and_get_response(user_phone, content, logger, settings.openai_api_key, settings.langchain_api_key, settings.cohere_api_key, True, db, redis_client)
+
 
 def is_emoji(character):
     emoji_pattern = re.compile(
@@ -218,48 +200,50 @@ def is_emoji(character):
     )
     return emoji_pattern.match(character)
 
+
 def split_paragraph_into_sentences(paragraph):
     sentence_endings = re.compile(r'([.!?])')
     sentences = sentence_endings.split(paragraph)
-    
+
     adjusted_sentences = []
     i = 0
-    
+
     while i < len(sentences) - 1:
         sentence = sentences[i]
         punctuation = sentences[i + 1]
         next_part = sentences[i + 2].lstrip() if i + 2 < len(sentences) else ""
-        
+
         if next_part and is_emoji(next_part[0]):
             sentence += punctuation + ' ' + next_part[0]
             if len(next_part) > 1:
                 sentences[i + 2] = next_part[1:]
             else:
                 i += 1  # Skip the next part as it is fully consumed
-        
+
         else:
             sentence += punctuation
-        
+
         adjusted_sentences.append(sentence.strip())
         i += 2
-    
+
     # Append any remaining parts
     if i < len(sentences):
         adjusted_sentences.append(sentences[-1].strip())
-    
+
     return adjusted_sentences
 
+
 async def send_message(user_phone: str, response: str, settings: config.Settings):
-    #split the response into multiple messages by sentences which can end with a period, question mark, or exclamation mark
-    
+    # split the response into multiple messages by sentences which can end with a period, question mark, or exclamation mark
+
     sentences = split_paragraph_into_sentences(response)
 
-    #check if the last sentence is an emoji
- 
+    # check if the last sentence is an emoji
 
     for i, sentence in enumerate(sentences):
         print(f"Sentence {i+1}: {sentence}")
 
+    res = []
     for sentence in sentences:
         headers = {
             "sb-api-key-id": settings.sendblue_apikey,
@@ -272,4 +256,5 @@ async def send_message(user_phone: str, response: str, settings: config.Settings
         }
         sendblue_response = requests.post(
             settings.sendblue_apiurl, headers=headers, json=data)
-    return sendblue_response.json
+        res.append(sendblue_response)
+    return res
--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
-from functools import lru_cache
+from datetime import timedelta
+from functools import lru_cache, wraps
+import json
 from typing import Annotated
 from dotenv import load_dotenv
+from contextlib import asynccontextmanager
 import logging
+import redis
 
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
@@ -14,9 +18,21 @@ models.Base.metadata.create_all(bind=engine)
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-app = FastAPI()
 
-load_dotenv()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # code to run on startup
+    load_dotenv()
+    # Redis cache initialization
+    app.state.redis_client = redis.Redis(
+        host='localhost', port=6379, db=0, decode_responses=True)
+    logger.info(f"Redis connection details: {
+                app.state.redis_client.connection_pool.connection_kwargs}")
+    yield
+    # code to run on shutdown
+
+app = FastAPI(lifespan=lifespan)
 
 
 @lru_cache
@@ -32,8 +48,48 @@ def get_db():
         db.close()
 
 
+def get_redis_client():
+    return app.state.redis_client
+
+
+def cache_entry(redis_client: redis.Redis, user_phone: str, content: str, response: str, date_sent: str) -> bool:
+    try:
+        key = f"user:{user_phone}:entries"
+        entry_data = {
+            "message": content,
+            "response": response,
+            "date_sent": date_sent
+        }
+        entry_json = json.dumps(entry_data)
+
+        expire_time = int(timedelta(hours=24).total_seconds())
+        # using Redis pipeline for atomic writes
+        with redis_client.pipeline() as pipe:
+            pipe.lpush(key, entry_json)
+            pipe.ltrim(key, 0, 99)
+            pipe.expire(key, expire_time)
+            results = pipe.execute()
+        return True
+    except redis.RedisError as e:
+        logger.error(f"Error storing data in Redis cache: {e}")
+        return False
+
+
+def get_session_history(redis_client: redis.Redis, user_phone: str):
+    key = f"user:{user_phone}:entries"
+    try:
+        logger.info(f"KEY: {key}")
+        data = redis_client.lrange(key, 0, -1)
+        escaped_data = [entry.replace("{", "{{").replace("}", "}}")
+                        for entry in data]  # type: ignore
+        return escaped_data
+    except redis.RedisError as e:
+        logger.error(f"Error reading from Redis: {e}.")
+
+
 @app.post("/entries/")
-async def create_entry(payload: utils.MessagePayload, settings: Annotated[config.Settings, Depends(get_settings)], db: Session = Depends(get_db)):
+async def create_entry(payload: utils.MessagePayload, settings: Annotated[config.Settings, Depends(get_settings)],
+                       db: Session = Depends(get_db), redis_client: redis.Redis = Depends(get_redis_client)):
     logger.info("Received a webhook request from SendBlue.")
     user_phone, content, date_sent = payload.from_number, payload.content, payload.date_sent
     user = crud.get_user_by_phone_number(db, user_phone)
@@ -44,12 +100,20 @@ async def create_entry(payload: utils.MessagePayload, settings: Annotated[config
         db, user.id, tokenizer.embed(content))  # type: ignore
 
     response = helpers.generate_response(
-        user_phone, content, date_sent, logger, settings, db)
+        user_phone, content, date_sent, logger, settings, db, redis_client)
     entry_embedding = tokenizer.embed(content)
     emotion = tokenizer.emotion(content)
     entry_data = schemas.EntryCreate(
         content=content, embedding=entry_embedding, emotions=emotion)
-    new_entry = crud.create_user_entry(db, entry_data, user.id)  # type: ignore
+
+    cache_success = cache_entry(
+        redis_client, user_phone, content, response, date_sent)
+    logger.info(f"Cache operation {
+                'succeeded' if cache_success else 'failed'}.")
+
+    crud.create_user_entry(db, entry_data, user.id)  # type: ignore
+
+    await helpers.send_message(user_phone, response, settings)
     to_return = await helpers.send_message(user_phone, response, settings)
     return response
 
